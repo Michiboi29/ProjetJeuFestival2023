221ee2d2582cf71aa0fe7710aa9b74ffdd26037b
Alex Dawson <ALDAW3@ulaval.ca>
2023-01-22 16:20:06 -0500

    debug button and speed bugs

--- CourseChevauxArduino/src/main.cpp
+++ CourseChevauxArduino/src/main.cpp
@@ -8,10 +8,10 @@
 
 #define POS_INIT  0
 #define POS_FINAL 900
-#define MAX_SPEED 200
-#define MIN_SPEED 10
-#define MAX_ACCELERATION 100
-#define MIN_ACCELERATION 10
+#define MAX_SPEED 700
+#define MIN_SPEED 50
+#define MAX_ACCELERATION 50
+#define MIN_ACCELERATION 1
 
 // Define some steppers and the pins the will use
 AccelStepper stepper1(AccelStepper::DRIVER, PIN_STEP1, PIN_DIR1);
@@ -35,21 +35,25 @@ void setMaxSpeed(){
   stepper1.setAcceleration(MAX_ACCELERATION);
 }
 
-void setup()
-{  
-    Serial.begin(9600);
-    stepper1.setEnablePin(PIN_EN1);
-    stepper1.disableOutputs();
-    stepper1.setMaxSpeed(MAX_SPEED);
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
-    
-    setMaxSpeed();
-    stepper1.enableOutputs();
-    stepper1.moveTo(POS_INIT);
-    delay(1000);
-    stepper1.disableOutputs();
+void goEnd(){
+  setRandomSpeed();
+  stepper1.enableOutputs();
+  stepper1.moveTo(POS_FINAL);
+  Serial.println("bouton go boom");
+  delay(200);
+  stepper1.disableOutputs();
 }
 
+void goStart(){
+  setMaxSpeed();
+  stepper1.enableOutputs();
+  stepper1.moveTo(POS_INIT);
+  Serial.println("bouton go back");
+  delay(500);
+  stepper1.disableOutputs();
+}
+
+bool toogleButts = false;
 bool buttonState = false;
 bool buttonLastState = false;
 void buttonToogle(){
@@ -62,13 +66,30 @@ void buttonToogle(){
   }
    
   if(buttonState && !buttonLastState){
-    setRandomSpeed();
-    stepper1.enableOutputs();
-    stepper1.moveTo(POS_FINAL);
-    Serial.println("bouton go boom");
+    toogleButts = !toogleButts;
+    if (toogleButts){
+      goEnd();
+    }
+    else{
+      goStart();
+    }
+    delay(125);
   }
   
 }
+
+void setup()
+{  
+    Serial.begin(9600);
+    stepper1.setEnablePin(PIN_EN1);
+    stepper1.disableOutputs();
+    stepper1.setMaxSpeed(MAX_SPEED);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    
+    goStart();
+}
+
+
 
 void loop()
 {
