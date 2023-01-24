6ac8b42fb5d9c80f879285297bc30287db2e8a6d
Alex Dawson <ALDAW3@ulaval.ca>
2023-01-24 12:33:25 -0500

    4 motors

--- CourseChevauxArduino/src/main.cpp
+++ CourseChevauxArduino/src/main.cpp
@@ -2,55 +2,102 @@
 #include <AccelStepper.h>
 
 #define BUTTON_PIN 22
-#define PIN_STEP1  4
-#define PIN_DIR1   5
-#define PIN_EN1    6
+#define PIN_STEP1  23
+#define PIN_DIR1   24
+#define PIN_EN1    25
+#define PIN_STEP2  26
+#define PIN_DIR2   27
+#define PIN_EN2    28
+#define PIN_STEP3  29
+#define PIN_DIR3   30
+#define PIN_EN3    31
+#define PIN_STEP4  32
+#define PIN_DIR4   33
+#define PIN_EN4    34
 
 #define POS_INIT  0
-#define POS_FINAL 900
-#define MAX_SPEED 700
-#define MIN_SPEED 50
-#define MAX_ACCELERATION 50
+#define POS_FINAL 16000
+#define MAX_SPEED 300
+#define MIN_SPEED 30
+#define MAX_ACCELERATION 30
 #define MIN_ACCELERATION 1
 
 // Define some steppers and the pins the will use
 AccelStepper stepper1(AccelStepper::DRIVER, PIN_STEP1, PIN_DIR1);
-
-void test(){
-  stepper1.setMaxSpeed(200.0);
-  stepper1.setSpeed(50);
-  stepper1.setAcceleration(100.0);
-  stepper1.moveTo(24);
-}
+AccelStepper stepper2(AccelStepper::DRIVER, PIN_STEP2, PIN_DIR2);
+AccelStepper stepper3(AccelStepper::DRIVER, PIN_STEP3, PIN_DIR3);
+AccelStepper stepper4(AccelStepper::DRIVER, PIN_STEP4, PIN_DIR4);
 
 void setRandomSpeed(){
-  int speed = random(MIN_SPEED,MAX_SPEED);
-  int acceleration = random(MIN_ACCELERATION,MAX_ACCELERATION);
-  stepper1.setSpeed(speed);
-  stepper1.setAcceleration(acceleration);
+  int speed1 = random(MIN_SPEED,MAX_SPEED);
+  int acceleration1 = random(MIN_ACCELERATION,MAX_ACCELERATION);
+  stepper1.setSpeed(speed1);
+  stepper1.setAcceleration(acceleration1);
+
+  int speed2 = random(MIN_SPEED,MAX_SPEED);
+  int acceleration2 = random(MIN_ACCELERATION,MAX_ACCELERATION);
+  stepper2.setSpeed(speed2);
+  stepper2.setAcceleration(acceleration2);
+
+  int speed3 = random(MIN_SPEED,MAX_SPEED);
+  int acceleration3 = random(MIN_ACCELERATION,MAX_ACCELERATION);
+  stepper3.setSpeed(speed3);
+  stepper3.setAcceleration(acceleration3);
+
+  int speed4 = random(MIN_SPEED,MAX_SPEED);
+  int acceleration4 = random(MIN_ACCELERATION,MAX_ACCELERATION);
+  stepper4.setSpeed(speed4);
+  stepper4.setAcceleration(acceleration4);
 }
 
 void setMaxSpeed(){
   stepper1.setSpeed(MAX_SPEED);
-  stepper1.setAcceleration(MAX_ACCELERATION);
+  stepper1.setAcceleration(MIN_ACCELERATION);
+
+  stepper2.setSpeed(MAX_SPEED);
+  stepper2.setAcceleration(MIN_ACCELERATION);
+
+  stepper3.setSpeed(MAX_SPEED);
+  stepper3.setAcceleration(MIN_ACCELERATION);
+
+  stepper4.setSpeed(MAX_SPEED);
+  stepper4.setAcceleration(MIN_ACCELERATION);
 }
 
 void goEnd(){
   setRandomSpeed();
   stepper1.enableOutputs();
   stepper1.moveTo(POS_FINAL);
+  stepper2.enableOutputs();
+  stepper2.moveTo(POS_FINAL);
+  stepper3.enableOutputs();
+  stepper3.moveTo(POS_FINAL);
+  stepper4.enableOutputs();
+  stepper4.moveTo(POS_FINAL);
   Serial.println("bouton go boom");
   delay(200);
   stepper1.disableOutputs();
+  stepper2.disableOutputs();
+  stepper3.disableOutputs();
+  stepper4.disableOutputs();
 }
 
 void goStart(){
   setMaxSpeed();
   stepper1.enableOutputs();
   stepper1.moveTo(POS_INIT);
+  stepper2.enableOutputs();
+  stepper2.moveTo(POS_INIT);
+  stepper3.enableOutputs();
+  stepper3.moveTo(POS_INIT);
+  stepper4.enableOutputs();
+  stepper4.moveTo(POS_INIT);
   Serial.println("bouton go back");
   delay(500);
   stepper1.disableOutputs();
+  stepper2.disableOutputs();
+  stepper3.disableOutputs();
+  stepper4.disableOutputs();
 }
 
 bool toogleButts = false;
@@ -81,11 +128,24 @@ void buttonToogle(){
 void setup()
 {  
     Serial.begin(9600);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+
     stepper1.setEnablePin(PIN_EN1);
     stepper1.disableOutputs();
     stepper1.setMaxSpeed(MAX_SPEED);
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
     
+    stepper2.setEnablePin(PIN_EN1);
+    stepper2.disableOutputs();
+    stepper2.setMaxSpeed(MAX_SPEED);
+
+    stepper3.setEnablePin(PIN_EN1);
+    stepper3.disableOutputs();
+    stepper3.setMaxSpeed(MAX_SPEED);
+
+    stepper4.setEnablePin(PIN_EN1);
+    stepper4.disableOutputs();
+    stepper4.setMaxSpeed(MAX_SPEED);
+
     goStart();
 }
 
@@ -95,4 +155,7 @@ void loop()
 {
     buttonToogle();
     stepper1.run();
+    stepper2.run();
+    stepper3.run();
+    stepper4.run();
 }
